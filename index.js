45d9d08ccf1f8ad85ce1519c9594444a47ae925a
Roushan kumar <roushankumar7231246@gmail.com>
2023-01-10 23:09:43 +0530

    create home ejs file and render it

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use('/', require('./routes'));
 
 //set up the view engine
 app.set('view engine', 'ejs');
-app.set('view','./views');
+app.set('views','./views');
 
 
 app.listen(port, function(err){
