36b69bdd99ee4a506db4f1d29d7ccf8701eeb949
Roushan kumar <roushankumar7231246@gmail.com>
2023-01-10 22:59:17 +0530

    set view engine

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.use('/', require('./routes'));
 
 
 
+//set up the view engine
+app.set('view engine', 'ejs');
+app.set('view','./views');
+
+
 app.listen(port, function(err){
     if(err){
       console.log("Error: ", err);  
