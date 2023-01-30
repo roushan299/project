9a0e56e2cf68cf056e1f5590b6850c688fee722f
Roushan kumar <roushankumar7231246@gmail.com>
2023-01-30 09:17:40 +0530

    user name and email updating file created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -79,3 +79,14 @@ module.exports.destroySession = function(req, res, next){
     });
     return res.redirect('/');
 }
+
+
+module.exports.update = function(req, res){
+    if(req.user.id == req.params.id){
+    User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+        return res.redirect('back');
+    });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const usersController = require('../controllers/users_controller');
 
 
 router.get('/profile/:id',passport.checkAuthentication, usersController.profile);
+router.post('/update/:id', passport.checkAuthentication, usersController.update);
 router.use('/', require('./hii'));
 
 router.get('/sign-in', usersController.sigin);
