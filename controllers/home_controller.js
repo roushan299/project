648d2c5d4e3c37a2601de2604cd543e1e0a80345
Roushan kumar <roushankumar7231246@gmail.com>
2023-02-02 08:45:04 +0530

    all database retirival and destroying data code are converted into async function

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,46 +1,62 @@
 const User = require('../models/user');
 const Post = require('../models/post');
 
-module.exports.home = function(req, res){
-    //console.log(req.cookies);
-    //res.cookie('user_id',5);
-//     if(req.isAuthenticated()){
-//     return res.render('home',{
-//         title:"FoodRunner | Dashboard"
+// module.exports.home = function(req, res){
+//     //console.log(req.cookies);
+//     //res.cookie('user_id',5);
+// //     if(req.isAuthenticated()){
+// //     return res.render('home',{
+// //         title:"FoodRunner | Dashboard"
+// //     });
+// // }
+// //     return res.redirect('/users/sign-in');
+
+//     // Post.find({}, function(err, posts){
+//     //     if(err){
+//     //         console.log('!!!Error in fetching posts from db');
+//     //         return res.redirect('back');
+//     //     }
+//     //     return res.render('home', {
+//     //         title:'FoodRunner | Dashboard',
+//     //         posts:posts
+//     //     });
+//     // });
+
+//     //populate the user and the posts so that user name can be shown
+//     Post.find({})
+//     .populate('user')
+//     .populate({
+//         path:'comments',
+//         populate:{
+//             path:'user'
+//         }
+//     })
+//     .exec(function(err, posts){
+//         User.find({}, function(err, user){
+//             return res.render('home', {
+//                 title:"FoodRunner | Dashboard",
+//                 posts:posts,
+//                 all_users:user
+//             });
+//         });
+        
 //     });
 // }
-//     return res.redirect('/users/sign-in');
+module.exports.home = async function(req, res){
+    try {
+        let post = await Post.find({}).populate('user').populate({path:'comments',populate:{path: 'user'}});
 
-    // Post.find({}, function(err, posts){
-    //     if(err){
-    //         console.log('!!!Error in fetching posts from db');
-    //         return res.redirect('back');
-    //     }
-    //     return res.render('home', {
-    //         title:'FoodRunner | Dashboard',
-    //         posts:posts
-    //     });
-    // });
+        let users = await User.find({});
 
-    //populate the user and the posts so that user name can be shown
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err, posts){
-        User.find({}, function(err, user){
-            return res.render('home', {
-                title:"FoodRunner | Dashboard",
-                posts:posts,
-                all_users:user
-            });
+        return res.render('home', {
+            title: "FoodRunner | Dashboard",
+            posts: post,
+            all_users:users
         });
-        
-    });
+    }
+    catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
-
 //module.exports.actionName = function(req, res){}
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,28 +1,31 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req, res){
-    Post.create({
+module.exports.create = async function(req, res){
+    try {
+   let post = await Post.create({
         content:req.body.content,
         user:req.user._id
-    }, function(err){
-        if(err){
-            console.log('!!!Error in creating post');
-            return res.redirect('back');
-        }
-        return res.redirect('back');
     });
+    return res.redirect('back');
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
 
-module.exports.destroy =function(req, res){
-    Post.findById(req.params.id, function(err, post){
+module.exports.destroy = async function(req, res){
+    try{
+     let post = await Post.findById(req.params.id);
         if(post.user==req.user.id){
             post.remove();
-            Comment.deleteMany({post:req.params.id}, function(err){
+           await Comment.deleteMany({post:req.params.id});
                 return res.redirect('back');
-            });
         }else{
             return res.redirect('back');
         }
-    });
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
